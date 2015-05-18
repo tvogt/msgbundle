263d1d3001281446b05f9b0c168eae8ca978232d
Tom Vogt <tom@lemuria.org>
2015-05-18 08:23:47 +0200

    bugfixes

--- Controller/ManageController.php
+++ Controller/ManageController.php
@@ -5,6 +5,7 @@ namespace Calitarus\MessagingBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
@@ -33,8 +34,35 @@ class ManageController extends Controller {
 		$em = $this->getDoctrine()->getManager();
 		$rights = $em->getRepository('MsgBundle:Right')->findAll();
 
-		return array('metas'=>$metas, 'rights'=>$rights);
+		return array('metas'=>$metas, 'rights'=>$rights, 'my_meta'=>$meta);
 	}
+
+	/**
+		* @Route("/participants_remove", name="cmsg_participant_remove")
+		*/
+	public function participantsremoveAction(Request $request) {
+		$user = $this->get('message_manager')->getCurrentUser();
+		$em = $this->getDoctrine()->getManager();
+		$meta = $em->getRepository('MsgBundle:ConversationMetadata')->find($request->request->get('meta'));
+
+		if (!$meta || $meta->getUser() != $user) {
+			throw new AccessDeniedHttpException($this->get('translator')->trans('error.conversation.noaccess', array(), "MsgBundle"));
+		}
+		if (!$meta->hasRightByName('remove') && !$meta->hasRightByName('owner')) {
+			throw new AccessDeniedHttpException($this->get('translator')->trans('error.conversation.noright', array(), "MsgBundle"));			
+		}
+
+		$target = $em->getRepository('MsgBundle:User')->find($request->request->get('id'));
+		$target_meta = $meta->getConversation()->findMeta($target);
+		if ($target_meta && !$target_meta->hasRightByName('owner')) {
+			$this->get('message_manager')->leaveConversation($target_meta, $target);
+			$em->flush();
+			return new JsonResponse(true); 
+		} else {
+			return new JsonResponse(false);
+		}
+	}
+
 
 	/**
 		* @Route("/conversation/leave", name="cmsg_leave", defaults={"_format"="json"})
